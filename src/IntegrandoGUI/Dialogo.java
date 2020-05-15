e7c87c1f7bc12c71de863e88b233681e11dd5c0f
Jhonatan-Cortezz <jhonatancortez708@gmail.com>
2020-05-15 13:38:59 -0600

    Primer mensaje con JoptionPane

---
+++ src/IntegrandoGUI/Dialogo.java
@@ -0,0 +1,14 @@
+
+package IntegrandoGUI;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JHONATAN CORTEZ
+ */
+public class Dialogo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido a Java");
+    }
+}
